3474c810ecb4453d4f595b877cd3c4ef0507ffda
Chun Xu <cxu169@gmail.com>
2023-09-11 00:59:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+import torch
+
 N_SUBJECTS = 678
 N_ROI = 35
 # N_FEATURE = 595
 N_FEATURE = int(N_ROI * (N_ROI - 1) / 2)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 save_path = "saved_trained/"
 
+# parameters
+fed_rounds = 10
+epochs_per_round = 10
+batch_size = 32
+c_idx = 0
+kl = False
+topo = False
+lamb = [0.1, 0.1]
